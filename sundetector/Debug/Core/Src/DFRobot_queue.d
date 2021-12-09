fc2fe308964c605bd3dd5886d85c0e1ad855c0b0
woogyeonghee <dnrudgml119@naver.com>
2021-12-09 21:37:38 +0900

    del sundetect basic

--- sundetector/Debug/Core/Src/DFRobot_queue.d
+++
@@ -1,4 +0,0 @@
-Core/Src/DFRobot_queue.o: ../Core/Src/DFRobot_queue.c \
- ../Core/Inc/DFRobot_queue.h
-
-../Core/Inc/DFRobot_queue.h:
--- sundetector/Debug/Core/Src/rb.d
+++
@@ -1,3 +0,0 @@
-Core/Src/rb.o: ../Core/Src/rb.c ../Core/Inc/rb.h
-
-../Core/Inc/rb.h:
